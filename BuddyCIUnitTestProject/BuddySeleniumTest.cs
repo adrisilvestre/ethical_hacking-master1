b2ae769b8fececb5dda8f0e6b0f28a14906feb8e
Adrián Silvestre <adrisilvs@gmail.com>
2019-10-02 01:43:52 -0400

    Selenium Added
    Selenium + 1 Unit Test

---
+++ BuddyCIUnitTestProject/BuddySeleniumTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace BuddyCIUnitTestProject
+{
+    [TestClass]
+    public class BuddySeleniumTest
+    {
+        ChromeDriver Driver { get; set; }
+        WebDriverWait Wait { get; set; }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+                string url = "http://localhost:53557/";
+                Driver = new ChromeDriver();
+                Driver.Navigate().GoToUrl(url);
+                Driver.Manage().Window.Maximize();
+                Driver.FindElementById("ft_un").SendKeys("1070824");
+                Driver.FindElementById("ft_pd").SendKeys("5545682");
+                Driver.Close();
+                Driver.Dispose();
+            }
+            catch
+            {
+                ITakesScreenshot screenshotDriver = Driver as ITakesScreenshot;
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+                Driver.Quit();
+            }
+        }
+    }
+}
---
+++ BuddyCIUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BuddyCIUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("BuddyCIUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30df8b27-8999-4423-bfc3-4ad094b75d70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
