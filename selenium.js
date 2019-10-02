de6cbf27e81afead830d9fada8649f8970b67cd8
Adrián Silvestre <adrisilvs@gmail.com>
2019-10-02 18:43:15 -0400

    Update selenium.js

--- selenium.js
+++ selenium.js
@@ -20,9 +20,9 @@ try{webdriverio2
     .remote(options)
     .init()
     .url('https://prueba-ci-cd.azurewebsites.net/')
-    .findElement(By.id('ft_un')).sendKeys('1070824')
-    .findElement(By.id('ft_pd')).sendKeys('5545682')
-    .findElement(By.id('submitButton')).Click()
+    .findElement(Webdriver.By.id('ft_un')).sendKeys('1070824')
+    .findElement(Webdriver.By.id('ft_pd')).sendKeys('5545682')
+    .findElement(Webdriver.By.id('submitButton')).Click()
     .manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
     .end();}finally{
         
