b21fbdb18d43b7167bf63a8103f0ccfd9f82021a
Adrián Silvestre <adrisilvs@gmail.com>
2019-10-02 19:45:13 -0400

    Update selenium.js

--- selenium.js
+++ selenium.js
@@ -13,17 +13,3 @@ try{webdriverio
     .end();}finally {
         // Close the browser.
         }
-
-
-
-try{webdriverio2
-    .remote(options)
-    .init()
-    .url('https://prueba-ci-cd.azurewebsites.net/')
-    .findElement(Webdriver.By.id('ft_un')).sendKeys('1070824')
-    .findElement(Webdriver.By.id('ft_pd')).sendKeys('5545682')
-    .findElement(Webdriver.By.id('submitButton')).Click()
-    .manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
-    .end();}finally{
-        
-    }
